80f4f32a25ca1c062cdc623205eb3e8b02fe2321
Diego448 <diegosg448@gmail.com>
2022-04-08 01:26:59 -0500

    Routes refactoring

--- backend/src/doofman/app.py
+++ backend/src/doofman/app.py
@@ -5,21 +5,21 @@ from models import User, UserUpdate
 app = FastAPI()
 
 @app.get('/user/all')
-async def get_all_users():
+async def get_users():
     return get_all('users')
 
 @app.get('/user/{user_id}')
-async def user_get(user_id: str):
+async def get_user(user_id: str):
     return get(user_id, 'users')
 
 @app.post('/user/add')
 async def add_user(user: User):
     return add(user, 'users')
 
-@app.put('/user/edit/{user_id}')
-async def user_edit(user_id: str, user_update: UserUpdate):
+@app.put('/user/update/{user_id}')
+async def update_user(user_id: str, user_update: UserUpdate):
     return update(user_id, user_update, 'users')
 
-@app.delete('/user/remove/{user_id}')
-async def remove_user(user_id: str):
+@app.delete('/user/delete/{user_id}')
+async def delete_user(user_id: str):
     return delete(user_id, 'users')
