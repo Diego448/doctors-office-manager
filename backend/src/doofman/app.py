f07dbefcf00dda4ffa10e344f7a5eccf0dbeb197
Diego448 <diegosg448@gmail.com>
2022-04-08 01:37:59 -0500

    Adding patient endpoints

--- backend/src/doofman/app.py
+++ backend/src/doofman/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from db_utils import add, get, get_all, update, delete
-from models import User, UserUpdate
+from models import User, UserUpdate, Patient, PatientUpdate
 
 app = FastAPI()
 
@@ -23,3 +23,23 @@ async def update_user(user_id: str, user_update: UserUpdate):
 @app.delete('/user/delete/{user_id}')
 async def delete_user(user_id: str):
     return delete(user_id, 'users')
+
+@app.get('/patient/all')
+async def get_patients():
+    return get_all('patients')
+
+@app.get('/patient/{patient_id}')
+async def get_patient(patient_id: str):
+    return get(patient_id, 'patients')
+
+@app.post('/patient/add')
+async def add_patient(patient: Patient):
+    return add(patient, 'patients')
+
+@app.put('/patient/update/{patient_id}')
+async def update_patient(patient_id: str, patient_update: PatientUpdate):
+    return update(patient_id, patient_update, 'patients')
+
+@app.delete('/patient/delete/{patient_id}')
+async def delete_patient(patient_id: str):
+    return delete(patient_id, 'patients')
