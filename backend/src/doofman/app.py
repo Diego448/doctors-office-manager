a9841eb533ad3bb3ff862f193c1b0f8daeb165a9
Diego448 <diegosg448@gmail.com>
2022-04-09 18:18:20 -0500

    Allowing CORS requests

--- backend/src/doofman/app.py
+++ backend/src/doofman/app.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from db_utils import add, get, get_all, update, delete
 from models import User, UserUpdate, Patient, PatientUpdate
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/user/all')
 async def get_users():
