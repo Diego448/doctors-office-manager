5ebd3867608cc3351900515882ae4bb94357f30b
Diego448 <diegosg448@gmail.com>
2022-04-12 01:57:32 -0500

    Adding patient list component and styles

---
+++ doofman-web/src/components/PatientListItem.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+
+        props: {
+            patient: {
+              type: Object,
+              default: () => {return {}}
+            }
+        },
+        data() {
+            
+        },
+        methods: {
+
+        },
+        mounted() {
+
+        },
+        created() {
+
+        }
+    })
+</script>
+
+<template>
+  <div class="card w-75 mx-auto my-2" :class="patient.status == 2 ? 'border-warning': ''">
+    <h5 class="card-header">{{ patient.name }}</h5>
+    <div class="card-body">
+      <p class="my-0"><span class="fw-bold pe-1">E-mail:</span>{{ patient.email }}</p>
+      <p class="my-0"><span class="fw-bold pe-1">Telephone number:</span>{{ patient.telephone_number }}</p>
+      <a href="#" class="card-link">Appointments</a>
+      <a href="#" class="card-link">Consults</a>
+      <a href="#" class="card-link">Payments</a>
+    </div>
+    <div class="card-footer">
+      {{ patient.status == 1 ? "Active": "Inactive"}}
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
--- doofman-web/src/views/PatientsView.vue
+++ doofman-web/src/views/PatientsView.vue
@@ -1,37 +1,33 @@
 <script  lang="ts">
 
     import { defineComponent } from 'vue'
+    import PatientListItem from '../components/PatientListItem.vue'
 
     export default defineComponent({
-
-        props: {
-            
-        },
-        data() {
-            return {
-                patients: null
-            }
-        },
-        methods: {
-            async getPatients() {
-                this.patients = await (await fetch('http://127.0.0.1:8000/patient/all')).json()
-            }
-        },
-        mounted() {
-
-        },
-        created() {
-            this.getPatients()
+    props: {},
+    data() {
+        return {
+            patients: null
+        };
+    },
+    methods: {
+        async getPatients() {
+            this.patients = await (await fetch("http://127.0.0.1:8000/patient/all")).json();
         }
-    })
+    },
+    mounted() {
+    },
+    created() {
+        this.getPatients();
+    },
+    components: { PatientListItem }
+})
 
 </script>
 
 <template>
   <main>
     <h1>Patients</h1>
-    <li v-for="patient in patients">
-    {{ patient.name }}
-    </li>
+    <PatientListItem v-for="patient in patients" :patient="patient"/>
   </main>
 </template>
