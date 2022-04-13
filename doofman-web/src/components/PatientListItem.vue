07b8022e29fb2c29a8975a9a6a64ea0510f34595
Diego448 <diegosg448@gmail.com>
2022-04-13 02:30:44 -0500

    Improving styles and adding dynamic routing

--- doofman-web/src/components/PatientListItem.vue
+++ doofman-web/src/components/PatientListItem.vue
@@ -5,12 +5,13 @@
 
         props: {
             patient: {
-              type: Object,
-              default: () => {return {}}
+              type: Object
             }
         },
         data() {
-            
+          return {
+            id: this.patient?._id.$oid
+          }
         },
         methods: {
 
@@ -20,22 +21,34 @@
         },
         created() {
 
+        },
+        computed: {
+          getPatientStatus(): string {
+            return this.patient?.status == 1 ? "Active": "Inactive"
+          },
+          setStatusStyles(): string {
+            return this.patient?.status == 2 ? 'bg-warning': 'bg-primary'
+          }
         }
     })
 </script>
 
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
 <template>
-  <div class="card w-75 mx-auto my-2" :class="patient.status == 2 ? 'border-warning': ''">
+  <div class="card w-50 mx-auto my-2">
     <h5 class="card-header">{{ patient.name }}</h5>
     <div class="card-body">
       <p class="my-0"><span class="fw-bold pe-1">E-mail:</span>{{ patient.email }}</p>
       <p class="my-0"><span class="fw-bold pe-1">Telephone number:</span>{{ patient.telephone_number }}</p>
-      <a href="#" class="card-link">Appointments</a>
-      <a href="#" class="card-link">Consults</a>
-      <a href="#" class="card-link">Payments</a>
+      <RouterLink v-bind:to="'/appointment/' + id" class="card-link text-decoration-none">Appointments</RouterLink>
+      <RouterLink :to="'/consult/' + id" class="card-link text-decoration-none">Consults</RouterLink>
+      <RouterLink :to="'/payment/' + id" class="card-link text-decoration-none">Payments</RouterLink>
     </div>
     <div class="card-footer">
-      {{ patient.status == 1 ? "Active": "Inactive"}}
+      <span class="badge" :class="setStatusStyles">{{ getPatientStatus }}</span>
     </div>
   </div>
 </template>
--- doofman-web/src/views/PatientsView.vue
+++ doofman-web/src/views/PatientsView.vue
@@ -1,9 +1,9 @@
 <script  lang="ts">
 
-    import { defineComponent } from 'vue'
-    import PatientListItem from '../components/PatientListItem.vue'
+import { defineComponent } from 'vue'
+import PatientListItem from '../components/PatientListItem.vue'
 
-    export default defineComponent({
+export default defineComponent({
     props: {},
     data() {
         return {
@@ -15,19 +15,21 @@
             this.patients = await (await fetch("http://127.0.0.1:8000/patient/all")).json();
         }
     },
-    mounted() {
-    },
+    mounted() {},
     created() {
         this.getPatients();
     },
     components: { PatientListItem }
-})
+    })
 
 </script>
 
 <template>
   <main>
-    <h1>Patients</h1>
+    <h1 class="h1 text-center">Patients</h1>
     <PatientListItem v-for="patient in patients" :patient="patient"/>
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <button class="btn btn-primary" type="button">Add</button>
+    </div>
   </main>
 </template>
